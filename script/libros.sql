f5016fac37f0e098a672fdddd49d42425bddd6f9
sandro_s_romero <romerosanseb@gmail.com>
2024-08-18 20:15:51 -0300

    se actualizo libros.sql

--- script/libros.sql
+++ script/libros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-08-2024 a las 03:25:07
+-- Tiempo de generación: 19-08-2024 a las 01:13:32
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
